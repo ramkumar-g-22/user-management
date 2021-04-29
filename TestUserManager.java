9633225c7ac7e782e410d6c0a54e0ee010f41efd
ramkumar-g-22 <82449964+ramkumar-g-22@users.noreply.github.com>
2021-04-29 21:19:16 +0530

    Add files via upload

--- TestUserManager.java
+++ TestUserManager.java
@@ -1,34 +1,159 @@
+package in.ramkumar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class TestUserManager {
-	public static void main(String[] args) {
-		
-		User user1 = new User("Ramkumar", "ram@mail.com", "1234");
-		User user2 = new User("Virat", "virat@mail.com", "8575");
-		User user3 = new User("John", "john@mail.com", "9202");
 
-		UserManager.login("Ramkumar");
-		
-		UserManager.displayUsers();
-		
-		UserManager.addUser(user1);
-		UserManager.addUser(user2);
-		UserManager.addUser(user3);
-		
-		UserManager.login("Ramkumar");
-		UserManager.displayUsers();
-
-		UserManager.changeUserName("Ramkumar", "Ram");
-		UserManager.displayUsers();
-		
-		UserManager.changePassword("John", "john123");		
-		UserManager.displayUsers();
-		
-		UserManager.changeEmail("John", "john123@gmail.com");
-		UserManager.displayUsers();
-
-
-		UserManager.removeUser("John");
-		UserManager.displayUsers();
-
+	@Test
+	public void testAddUserWithValidInput() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		boolean isValidUser = UserManager.addUser(user);
+		assertTrue(isValidUser);
 	}
+	
+	@Test
+	public void testAddUserWithInValidName() {
+		User user = new User(null, "ram@mail.com", "123");
+		boolean isValidUser = UserManager.addUser(user);
+		assertFalse(isValidUser);
+	}
+	
+	@Test
+	public void testAddUserWithInValidEmail() {
+		User user = new User("John1", null, "123");
+		boolean isValidUser = UserManager.addUser(user);
+		assertFalse(isValidUser);
+	}
+	
+	@Test
+	public void testAddUserWithInValidPassword() {
+		User user = new User("John2", "john@mail.com", null);
+		boolean isValidUser = UserManager.addUser(user);
+		assertFalse(isValidUser);
+	}
+	
+	@Test
+	public void testAddUserWithInValidNameAndPassword() {
+		User user = new User(null, "john@mail.com", null);
+		boolean isValidUser = UserManager.addUser(user);
+		assertFalse(isValidUser);
+	}
+	
+	@Test
+	public void testAddUserWithInValidNameAndEmail() {
+		User user = new User(null, null, "pass1234");
+		boolean isValidUser = UserManager.addUser(user);
+		assertFalse(isValidUser);
+	}
+	
+	@Test
+	public void testAddUserWithInValidPasswordAndEmail() {
+		User user = new User("Jay", null, null);
+		boolean isValidUser = UserManager.addUser(user);
+		assertFalse(isValidUser);
+	}
+	
+	@Test
+	public void testDisplayUsers() {
+		UserManager.displayUsers();
+	}
+	
+	@Test
+	public void testRemoveValidUser() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);	
+		boolean isUserRemoved = UserManager.removeUser("Ramkumar");
+		assertTrue(isUserRemoved);
+	}
+	
+	@Test
+	public void testRemoveInValidUser() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);	
+		boolean isUserRemoved = UserManager.removeUser("Ram");
+		assertFalse(isUserRemoved);
+	}
+	
+	@Test
+	public void testGetValidUserIndex() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);	
+		int userIndex = UserManager.getUserIndex("Ramkumar");
+		assertNotEquals(-1, userIndex);
+	}
+	
+	@Test
+	public void testGetInValidUserIndex() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);	
+		int userIndex = UserManager.getUserIndex("Ram");
+		assertEquals(-1, userIndex);
+	}
+	
+	@Test
+	public void testChangeValidUserName() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);
+		boolean isUserNameChanged = UserManager.changeUserName("Ramkumar", "Ram");
+		assertTrue(isUserNameChanged);
+	}
+	
+	@Test
+	public void testChangeInValidUserName() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);
+		boolean isUserNameChanged = UserManager.changeUserName("Ram", "John");
+		assertFalse(isUserNameChanged);
+	}
+	
+	@Test
+	public void testChangeValidPassword() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);
+		boolean isPasswordChanged = UserManager.changePassword("Ramkumar", "pass123");
+		assertTrue(isPasswordChanged);
+	}
+	
+	@Test
+	public void testChangeInValidPassword() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);
+		boolean isPasswordChanged = UserManager.changePassword("Ram", "John");
+		assertFalse(isPasswordChanged);
+	}
+	
+	@Test
+	public void testChangeValidEmail() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);
+		boolean isEmailChanged = UserManager.changeEmail("Ramkumar", "ramkumar@gmail.com");
+		assertTrue(isEmailChanged);
+	}
+	
+	@Test
+	public void testChangeInValidEmail() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);
+		boolean isEmailChanged = UserManager.changeEmail("Ram", "ramkumar@gmail.com");
+		assertFalse(isEmailChanged);
+	}
+	
+	@Test
+	public void testLoginWithValidUser() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);
+		boolean isValidUser = UserManager.login("Ramkumar");
+		assertTrue(isValidUser);
+	}
+	
+	@Test
+	public void testLoginWithInValidUser() {
+		User user = new User("Ramkumar", "ram@mail.com", "123");
+		UserManager.addUser(user);
+		boolean isValidUser = UserManager.login("Ram");
+		assertFalse(isValidUser);
+	}
+	
 }
--- UserManager.java
+++ UserManager.java
@@ -1,14 +1,25 @@
+package in.ramkumar;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserManager {
+	
 	static List<User> userList = new ArrayList<User>();
 	
-	public static void addUser(User user) {
-		userList.add(user);
-		System.out.println("User added successfully");
+	public static boolean addUser(User user) {
+		boolean validUser = false;
+		if (user.name != null  && user.email != null && user.password != null) {
+			userList.add(user);
+			validUser = true;
+			System.out.println("User added successfully : " + user.name);
+		}else {
+			System.out.println("Not a valid user : " + user.name);
+		}
+		return validUser;
 	}
 	
+
 	public static void displayUsers() {
 		System.out.println("User lists : " + userList.size());
 		for (User user : userList) {
@@ -16,17 +27,21 @@ public class UserManager {
 		}
 	}
 	
-	public static void removeUser(String name) {
+	public static boolean removeUser(String name) {
+		boolean validuser = false;
 		int userIndex = getUserIndex(name);
 		if (userIndex != -1) {
 			userList.remove(userIndex);
+			validuser = true;
 			System.out.println("User successfully removed");
 		}else {
+			validuser = false;
 			System.out.println("User " + name + " doesn\'t exists");
 		}
+		return validuser;
 	}
 
-	private static int getUserIndex(String name) {
+	public static int getUserIndex(String name) {
 		int userIndex = -1;
 		for (User user : userList) {
 			if(user.name.equals(name)) {
@@ -38,52 +53,68 @@ public class UserManager {
 	}
 
 
-	public static void changeUserName(String oldUserName, String newUserName) {
+	public static boolean changeUserName(String oldUserName, String newUserName) {
+		boolean validUser = false;
 		int userIndex = getUserIndex(oldUserName);
 		if (userIndex != -1) {
 			User user = userList.get(userIndex);
 			user.name = newUserName;
 			userList.set(userIndex, user);
+			validUser = true;
 			System.out.println("Username " + oldUserName + " is successfully changed to " + newUserName);
 		}else {
+			validUser = false;
 			System.out.println("User " + oldUserName  + " doesn\'t exists");
 		}
+		return validUser;
 	}
 
-	public static void changePassword(String userName, String newPassword) {
+	public static boolean changePassword(String userName, String newPassword) {
+		boolean validUser = false;
 		int userIndex = getUserIndex(userName);
 		if (userIndex != -1) {
 			User user = userList.get(userIndex);
 			user.password = newPassword;
 			userList.set(userIndex, user);
+			validUser = true;
 			System.out.println("Password changed successfully");
 		}else {
+			validUser = false;
 			System.out.println("User " + userName  + " doesn\'t exists");
 		}
+		return validUser;
 	}
 
 
-	public static void changeEmail(String userName, String newEmail) {
+	public static boolean changeEmail(String userName, String newEmail) {
+		boolean validUser = false;
 		int userIndex = getUserIndex(userName);
 		if (userIndex != -1) {
 			User user = userList.get(userIndex);
 			user.email = newEmail;
 			userList.set(userIndex, user);
+			validUser = true;
 			System.out.println("Email changed successfully");
 		}else {
+			validUser = false;
 			System.out.println("User " + userName  + " doesn\'t exists");
 		}
+		return validUser;
 	}
 
 	
-	public static void login(String userName) {
+	public static boolean login(String userName) {
 		// TODO Auto-generated method stub
+		boolean validUser = false;
 		int userIndex = getUserIndex(userName);
 		if (userIndex != -1) {
+			validUser = true;
 			System.out.println("Successfully logged in as " + userName);
 		}else {
+			validUser = false;
 			System.out.println("User " + userName  + " doesn\'t exists");
 		}
+		return validUser;
 
 	}
 	
